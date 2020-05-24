5166fce4b8ff6e1799f6960240c4e556a2422ee9
Volha Shved <volha.shved@rs-components.com>
2020-05-24 23:08:03 +0300

    solution to module-2 task-1-2

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -4,10 +4,24 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit;
+    this.bytes = 0;
   }
 
   _transform(chunk, encoding, callback) {
+    const data = chunk.toString();
+    this.bytes += data.length;
 
+    try {
+      if (this.bytes > this.limit) {
+        throw new LimitExceededError();
+      } else {
+        this.push(data);
+      }
+    } catch (err) {
+      return callback(err);
+    }
+    callback();
   }
 }
 
--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -4,12 +4,24 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.line = '';
+    this.data = [];
   }
 
   _transform(chunk, encoding, callback) {
+    const data = chunk.toString();
+    this.line += data;
+    this.data = this.line.split(os.EOL);
+    callback();
   }
 
   _flush(callback) {
+    for (let line of this.data) {
+      this.push(line); 
+    }
+    // This will be called when there is no more written data to be consumed,
+    // but before the 'end' event is emitted signaling the end of the Readable stream.
+    callback();
   }
 }
 
