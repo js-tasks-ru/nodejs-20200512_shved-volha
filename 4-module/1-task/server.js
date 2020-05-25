39cffcac00637dcaaa8067e1c8a01a5c4e0dcf81
Olga Shved <shved.volha@gmail.com>
2020-05-26 00:25:56 +0300

    solution to module-4 task-1

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,23 +1,34 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
-
+const fs = require('fs');
 const server = new http.Server();
-
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
-
+  const filepathArr = filepath.split('\\');
   switch (req.method) {
     case 'GET':
-
+      fs.exists(filepath, function(exists) {
+        if (exists) {
+          const rdStream = fs.createReadStream(filepath);
+          rdStream.on('data', function(chunk) {
+            res.end(chunk);
+          });
+        }
+        else {
+          if (filepathArr[filepathArr.length-2] !== 'files') {
+            res.statusCode = 400;
+          } else {
+            res.statusCode = 404;
+          }
+          res.end(`Error getting the file.`);
+        }
+      });
       break;
-
     default:
       res.statusCode = 501;
       res.end('Not implemented');
   }
 });
-
 module.exports = server;
