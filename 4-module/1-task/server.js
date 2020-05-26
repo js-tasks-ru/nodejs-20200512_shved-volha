f007969067b703fbc59e637cebfb466537830b35
Olga Shved <shved.volha@gmail.com>
2020-05-26 12:10:56 +0300

    solution 4-module 2-task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -2,11 +2,14 @@ const url = require('url');
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
+
 const server = new http.Server();
+
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
   const filepath = path.join(__dirname, 'files', pathname);
-  const filepathArr = filepath.split('\\');
+  const dirName = (path.dirname(pathname)).split();
+
   switch (req.method) {
     case 'GET':
       fs.exists(filepath, function(exists) {
@@ -17,7 +20,7 @@ server.on('request', (req, res) => {
           });
         }
         else {
-          if (filepathArr[filepathArr.length-2] !== 'files') {
+          if (dirName[0] !== '.') {
             res.statusCode = 400;
           } else {
             res.statusCode = 404;
@@ -25,7 +28,9 @@ server.on('request', (req, res) => {
           res.end(`Error getting the file.`);
         }
       });
+
       break;
+
     default:
       res.statusCode = 501;
       res.end('Not implemented');
--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -1,16 +1,44 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
+const limitedStream = new LimitSizeStream({limit: 1000000, encoding: 'utf-8'});
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
+  const dirName = (path.dirname(pathname)).split();
 
   switch (req.method) {
     case 'POST':
+      if (dirName[0] !== '.') {
+        res.statusCode = 400;
+        res.end('Nested path is not allowed');
+      } else {
+        fs.exists(filepath, function(exists) {
+          if (!exists) {
+            const writeStream = fs.createWriteStream(filepath);
+ 
+            limitedStream.pipe(writeStream);
+            writeStream.end();
+
+            limitedStream.on('error', (err) => {
+              res.statusCode = 413;
+              res.end('File size is too big');
+            });
+
+            writeStream.end();
+            res.statusCode = 201;
+            res.end('File successfully created');
+          } else {
+            res.statusCode = 409;
+            res.end(`File already exists`);
+          }
+        });
+      }
 
       break;
 
