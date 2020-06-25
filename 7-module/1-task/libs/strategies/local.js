15ca4629629c34f87cc66f66a52230d6db8c6da1
Volha Shved <volha.shved@rs-components.com>
2020-06-25 22:38:30 +0300

    8-module test commit

--- 7-module/1-task/libs/strategies/local.js
+++ 7-module/1-task/libs/strategies/local.js
@@ -1,24 +1,10 @@
 const LocalStrategy = require('passport-local').Strategy;
-<<<<<<< HEAD
-const User = require('../../models/User.js')
-=======
 const User = require('../../models/User.js');
->>>>>>> 2a5edfe25a6d285f35208c8c1ad76b0a11592285
 
 module.exports = new LocalStrategy(
     {usernameField: 'email', session: false},
     function(email, password, done) {
       User.findOne({email: email}, function(err, user) {
-<<<<<<< HEAD
-        if(err) { return done(err) };
-        if(!user) {
-          return done(null, false, 'Нет такого пользователя');
-        }
-        if(!user.checkPassword(password)) {
-          return done(null, false, 'Неверный пароль');
-        }
-        return done(null, user);
-=======
         if (err) {
           return done(err);
         };
@@ -33,7 +19,6 @@ module.exports = new LocalStrategy(
           };
           return done(null, user);
         });
->>>>>>> 2a5edfe25a6d285f35208c8c1ad76b0a11592285
       });
     }
 );
--- 8-module/1-task/controllers/registration.js
+++ 8-module/1-task/controllers/registration.js
@@ -3,7 +3,40 @@ const User = require('../models/User');
 const sendMail = require('../libs/sendMail');
 
 module.exports.register = async (ctx, next) => {
+  const { email, displayName, password } = ctx.user;
+  const user = await User.findOne({ email });
+
+  if(user) {
+    ctx.status = 400;
+    ctx.body = {errors: { email: 'Такой email уже существует' }};
+  }
+  else {
+    const verificationToken = uuid();
+    await User.create({ email, displayName, password, verificationToken });
+    //send email
+    await sendMail({
+      template: 'confirmation',
+      locals: {token: verificationToken},
+      to: email,
+      subject: 'Подтвердите почту',
+    });
+  }
+  return next();
 };
 
 module.exports.confirm = async (ctx, next) => {
+  const parsedUrl = ctx.request.url.split('/');
+  const verificationToken = parsedUrl[parsedUrl.length - 1];
+
+  const user = await User.findOne({ verificationToken });
+
+  if (!user){
+    ctx.status = 400;
+    ctx.body = {error: 'Ссылка подтверждения недействительна или устарела'};
+  }
+  else {
+    await User.update({verificationToken: ''});
+
+  }
+  return next();
 };
