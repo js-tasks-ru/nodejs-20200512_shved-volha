41f03f3da78662071a38e88496c5c917c5956b04
Olga Shved <shved.volha@gmail.com>
2020-06-26 17:47:03 +0300

    solution to 8-module 1-task

--- 8-module/1-task/controllers/registration.js
+++ 8-module/1-task/controllers/registration.js
@@ -3,40 +3,41 @@ const User = require('../models/User');
 const sendMail = require('../libs/sendMail');
 
 module.exports.register = async (ctx, next) => {
-  const { email, displayName, password } = ctx.user;
-  const user = await User.findOne({ email });
+  const { email, displayName, password } = ctx.request.body;
+  const user = await User.findOne({email});
 
-  if(user) {
+  if (user) {
     ctx.status = 400;
-    ctx.body = {errors: { email: 'Такой email уже существует' }};
-  }
-  else {
+    ctx.body = {errors: {email: 'Такой email уже существует'}};
+  } else {
     const verificationToken = uuid();
-    await User.create({ email, displayName, password, verificationToken });
-    //send email
+    const newUser = new User({email, displayName, verificationToken});
+    await newUser.setPassword(password);
+    await newUser.save();
+
+    // send email
     await sendMail({
       template: 'confirmation',
       locals: {token: verificationToken},
       to: email,
       subject: 'Подтвердите почту',
     });
+    ctx.body = {status: 'ok'};
   }
-  return next();
 };
 
 module.exports.confirm = async (ctx, next) => {
-  const parsedUrl = ctx.request.url.split('/');
-  const verificationToken = parsedUrl[parsedUrl.length - 1];
+  const { verificationToken } = ctx.request.body;
+  const user = await User.findOne({verificationToken});
 
-  const user = await User.findOne({ verificationToken });
-
-  if (!user){
+  if (!user) {
     ctx.status = 400;
     ctx.body = {error: 'Ссылка подтверждения недействительна или устарела'};
-  }
-  else {
-    await User.update({verificationToken: ''});
+  } else {
+    user.verificationToken = undefined;
+    await user.save();
+    const token = uuid();
 
+    ctx.body = {token};
   }
-  return next();
 };
